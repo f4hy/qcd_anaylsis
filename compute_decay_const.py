ad8cea8720ed8fd311d0afe5853e18ca12b30984
Brendan Fahy <bmfahy@gmail.com>
2015-05-27 14:56:24 +0900

    made residual mass a function

--- compute_decay_const.py
+++ compute_decay_const.py
@@ -7,6 +7,9 @@ import re
 import readinput
 import os
 
+from residualmasses import residual_mass
+
+
 def lines_without_comments(filename, comment="#"):
     from cStringIO import StringIO
     s = StringIO()
@@ -57,21 +60,6 @@ def read_file(filename):
     if filetype == "space_seperated":
         df = pd.read_csv(txt, delimiter=' ', names=names)
     return df
-
-
-def residual_mass(udmass, smass):
-    residual_masses = {(0.019, 0.030): 0.00047, (0.012, 0.030): 0.00040, (0.007, 0.030): 0.00042,
-                        (0.019, 0.040): 0.00041, (0.012, 0.040): 0.00044, (0.007, 0.040): 0.00038,
-                        (0.0035, 0.040): 0.00039,
-                        (0.008, 0.018): 0.00000, (0.008, 0.025): 0.00000,
-                        (0.012, 0.018): 0.000053, (0.012, 0.025): 0.000032,
-                        (0.0042, 0.018): 0.00000, (0.0042, 0.025): 0.00000 ,
-                        (0.0030, 0.0150): 0.00000}
-
-    try:
-        return residual_masses[(udmass, smass)]
-    except KeyError:
-        return 0.0
 
 
 def decay_constant(filename, options):
@@ -188,4 +176,6 @@ if __name__ == "__main__":
 
 
     for f in args.files:
+        if "2k-pi" in f:
+            continue
         decay_constant(f, args)
