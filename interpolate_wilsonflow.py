571f23bbe3e2e5353ed440c10161bcddceb80376
Brendan Fahy <bmfahy@gmail.com>
2015-06-11 19:40:37 +0900

    fix flycheck errors

--- interpolate_wilsonflow.py
+++ interpolate_wilsonflow.py
@@ -5,55 +5,34 @@ import numpy as np
 import pandas as pd
 import re
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-import matplotlib.colors as pltcolors
-from scipy.optimize import leastsq
-import glob
 from iminuit import Minuit
 
+from residualmasses import residual_mass
+
+from ensamble_info import data_params, read_fit_mass
+
 colors = ['b', 'k', 'c', 'y', 'm', 'b']
-#colors = ['c', 'm', 'k', 'm', 'c', 'y', 'b', 'r', 'k', 'm', 'c', 'y']
 
 # hbar*c / 1 fm  = 197.327 MeV
 hbar_c = 197.327
 
 
-from residualmasses import residual_mass
-
-from ensamble_info import flavor_map, scale, data_params, determine_flavor, read_fit_mass
-from ensamble_info import all_same_beta, all_same_heavy, all_same_flavor
-
-
 def read_files(files, xtype, fitdata):
     data = {}
 
-    N = None
-
     for f in files:
-        print f
+        logging.info("reading file {}".format(f))
         dp = data_params(f)
-
-        mud = dp.ud_mass
-        strange_mass = re.search("ms([a-z0-9].+)", f).group(1)
-        strange_mass = dp.s_mass
-
-
-        if strange_mass == "shifted":
-            strange_mass = "interpolated"
-
         df = pd.read_csv(f, comment='#', names=["flow"])
-
         xvalue = xvalues(xtype, dp, fitdata, t_0=np.mean(df.flow.values))
 
         data[dp] = (np.mean(xvalue), df.flow.values)
-
 
     return data
 
 
 def xvalues(xaxis_type, data_properties, fitdata, t_0=None):
     logging.info("using xaxis type {}".format(xaxis_type))
-
 
     if xaxis_type == "mud":
         residual = residual_mass(data_properties.ud_mass, data_properties.s_mass)
@@ -65,7 +44,7 @@ def xvalues(xaxis_type, data_properties, fitdata, t_0=None):
 
     if xaxis_type == "tmpisqr":
         pionmass = read_fit_mass(data_properties, "ud-ud", fitdata)
-        return ((t_0)*pionmass)**2 #*pionmass
+        return ((t_0)*pionmass)**2
 
     if xaxis_type == "t_2mksqr-mpisqr":
         pionmass = read_fit_mass(data_properties, "ud-ud", fitdata)
@@ -73,17 +52,14 @@ def xvalues(xaxis_type, data_properties, fitdata, t_0=None):
         return (t_0)**2*(2.0*(kaonmass)**2 + (pionmass)**2)
 
 
-
 def interpolate(data):
 
     logging.info("Fitting data")
-
 
     dps = []
     xvalues = []
     flows = []
     variances = []
-    fits = []
 
     for dp, d in data.iteritems():
         dps.append(dp)
@@ -91,7 +67,6 @@ def interpolate(data):
         flows.append(np.mean(d[1]))
         n = float(len(d[1]))
         variances.append(((n-1)*np.var(d[1])))
-
 
     xvalues = np.array(xvalues)
     flows = np.array(flows)
@@ -105,8 +80,6 @@ def interpolate(data):
 
     guess_A = np.mean(flows)
     guess_slope = (min(flows)-max(flows)) / (max(xvalues) - min(xvalues))
-
-    guess = (guess_slope, guess_A)
 
     m = Minuit(weighted_sqr_diff, C=guess_slope, error_C=guess_slope*0.01,
                A=guess_A, error_A=guess_A*0.01, errordef=dof,
@@ -123,13 +96,10 @@ def interpolate(data):
     return m
 
 
-
 def plot_fitline(data, fit_params, ftype, physical, outstub):
     logging.info("ploting")
-    size = 100
 
     c = colors.pop()
-    fontsettings = dict(fontsize=20)
     plotsettings = dict(linestyle="none", ms=8, elinewidth=3, capsize=8,
                         capthick=2, mew=3, aa=True, fmt='o')
 
@@ -141,7 +111,7 @@ def plot_fitline(data, fit_params, ftype, physical, outstub):
         y = np.mean(flow)
         err = np.sqrt((N-1)*(np.std(flow)**2))
         logging.info("{}, {}={}, err={}".format(dp, ftype, y, err))
-        patch = plt.errorbar(xvalue, y, yerr=err, color=c, ecolor=c, mec=c, **plotsettings)
+        # patch = plt.errorbar(xvalue, y, yerr=err, color=c, ecolor=c, mec=c, **plotsettings)
 
         xvalues.append(xvalue)
 
@@ -158,16 +128,15 @@ def plot_fitline(data, fit_params, ftype, physical, outstub):
     t3 = 2*xdata*mydata*m.covariance[("A", "C")]
     perry = t1+t2+t3
 
-    plt.fill_between(xdata, mydata, mydata+np.sqrt(perry),facecolor=c, alpha=0.1, lw=0, color=c, zorder=-10)
-    plt.fill_between(xdata, mydata, mydata-np.sqrt(perry),facecolor=c, alpha=0.1, lw=0, color=c, zorder=-10)
+    plt.fill_between(xdata, mydata, mydata+np.sqrt(perry), facecolor=c, alpha=0.1, lw=0, zorder=-10)
+    plt.fill_between(xdata, mydata, mydata-np.sqrt(perry), facecolor=c, alpha=0.1, lw=0, zorder=-10)
 
     physical_variance = (m.errors["A"]*(1+m.values["C"]*physical))**2
     physical_variance += ((m.values["A"])*physical*m.errors["C"])**2
     physical_variance += 2*physical*phys_y*m.covariance[("A", "C")]
 
-
-    plt.errorbar(physical, phys_y, yerr=np.sqrt(physical_variance), color="r", mec="r", **plotsettings)
-
+    plt.errorbar(physical, phys_y, yerr=np.sqrt(physical_variance),
+                 color="r", mec="r", **plotsettings)
 
     perror = np.sqrt(physical_variance)
 
@@ -176,22 +145,18 @@ def plot_fitline(data, fit_params, ftype, physical, outstub):
     return phys_y, perror
 
 
-
 def finish_plot(beta, ftype, xlabel, legend_handles, outstub):
     fontsettings = dict(fontsize=20)
-
-    plotsettings = dict(linestyle="none", ms=8, elinewidth=3, capsize=8,
-                    capthick=2, mew=3, aa=True, fmt='o')
 
     plt.title(r'$\beta={}$    ${}$'.format(beta, ftype), **fontsettings)
     plt.ylabel("${} / a$".format(ftype), **fontsettings)
     plt.xlabel(xlabel, **fontsettings)
 
-    plt.legend(handles=sorted(legend_handles), loc=0, **fontsettings )
+    plt.legend(handles=sorted(legend_handles), loc=0, **fontsettings)
 
     if outstub is not None:
         fig = plt.gcf()
-        fig.set_size_inches(18.5,10.5)
+        fig.set_size_inches(18.5, 10.5)
         filename = outstub+".png"
         logging.info("Saving plot to {}".format(filename))
         plt.savefig(filename, dpi=200)
@@ -209,12 +174,9 @@ def write_data(t0_ch, a_inv, a_inv_err, output_stub, suffix):
         ofile.write("{}, {} +/- {}\n".format(t0_ch, a_inv, a_inv_err))
 
 
-
 def interpolate_wilsonflow(options):
     """ script to interpolate the heavy mass """
     logging.debug("Called with {}".format(options))
-
-
 
     try:
         beta = re.search("_b(4\.[0-9]*)_", options.files[0]).group(1)
@@ -236,39 +198,37 @@ def interpolate_wilsonflow(options):
     if options.xaxis == "tmpisqr":
         physical = ((0.1465/hbar_c)*135.0)**2
     if options.xaxis == "t_2mksqr-mpisqr":
-        physical = ((0.1465/hbar_c)**2)*(2*(495**2)+ 138.0**2)
-
+        physical = ((0.1465/hbar_c)**2)*(2*(495**2) + 138.0**2)
 
     legend_handles = []
 
     stranges = set(i.s_mass for i in alldata.keys())
 
-
     if options.seperate_strange:
         logging.info("fitting strangeses seperately")
-        groups = [{k:v for k,v in alldata.iteritems() if k.s_mass == s} for s in stranges ]
+        groups = [{k: v for k, v in alldata.iteritems() if k.s_mass == s} for s in stranges]
     else:
         logging.info("fitting all data together")
         groups = [alldata]
-
 
     for data_group in groups:
 
         fit_params = interpolate(data_group)
 
-        fitstring = ", ".join(["{}: {:.6f} +/- {:.6f}".format(k,v,fit_params.errors[k]) for k,v in fit_params.values.iteritems()])
+        fitstring = ", ".join(["{}: {:.6f} +/- {:.6f}".format(k, v, fit_params.errors[k]) for
+                               k, v in fit_params.values.iteritems()])
         logging.info("fit parameters found to be {}".format(fitstring))
 
-        phys_t0 =  0.1465
+        phys_t0 = 0.1465
 
-        phys_y, phys_err = plot_fitline(data_group, fit_params, ftype, physical, options.output_stub)
+        phys_y, phys_err = plot_fitline(data_group, fit_params, ftype, physical,
+                                        options.output_stub)
 
         ainv = hbar_c * phys_y / phys_t0
         ainv_err = hbar_c * phys_err / phys_t0
         logging.info("ainv = {} +/- {}".format(ainv, ainv_err))
 
-        write_data(phys_y , ainv, ainv_err, options.output_stub, "_a_inv")
-
+        write_data(phys_y, ainv, ainv_err, options.output_stub, "_a_inv")
 
     xlabels = {"mud": r"$t_0^{1/2} m_{ud}$", "tmpisqr": r"$t_0 (m_{\pi})^2$",
                "t_2mksqr-mpisqr": r"$t_0 (2m_k^2+m_{\pi}^2)^2$"}
@@ -308,11 +268,9 @@ if __name__ == "__main__":
     else:
         logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
 
-
     if args.xaxis == "tmpisqr":
         for f in args.files:
             if "t0" not in f:
                 raise argparse.ArgumentTypeError("tmpisqr requires t0 data".format(f))
-
 
     interpolate_wilsonflow(args)
