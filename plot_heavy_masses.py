cd56b781851e36a10de166f8fa404bcfcf720047
Brendan Fahy <bmfahy@gmail.com>
2017-01-16 17:10:19 +0900

    add inverse masses

--- plot_heavy_masses.py
+++ plot_heavy_masses.py
@@ -10,3 +10,100 @@ def mD(ed, options):
         label += " [MeV]"
     return (data.mean(), data.std(),
                      label, {"Charm": phys_D, "Bottom": phys_MB})
+
+
+def inv_mhl(ed, options):
+    data = ed.get_mass("heavy-ud")
+    label = "$1/m_{hl}$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+    phys = {"Charm": phys_D, "Bottom": phys_MB}
+    return {m.split("_")[-1]: ((1/d).mean(), (1/d).std(), label, phys ) for m,d in data.iteritems()}
+
+
+def inv_mD(ed, options):
+    data = 1.0 / ed.D_mass()
+    label = "$1/m_{hl}$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+    return (data.mean(), data.std(),
+                     label, {"Charm": 1/phys_D, "Bottom": 1/phys_MB})
+
+
+def mDs(ed, options):
+    data = ed.Ds_mass()
+    label = "$m_{hs}$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+    return (data.mean(), data.std(),
+                     label, {"Charm": phys_Ds, "Bottom": phys_MBs})
+
+
+def inv_mhs(ed, options):
+    data = ed.get_mass("heavy-s")
+    label = "$1/m_{hs}$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+    phys = {"Charm": phys_Ds, "Bottom": phys_MBs}
+    return {m.split("_")[-1]: ((1/d).mean(), (1/d).std(), label, phys ) for m,d in data.iteritems()}
+
+
+def inv_mDs(ed, options):
+    data = 1.0 / ed.Ds_mass()
+    label = "$m_{hs}$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+    return (data.mean(), data.std(),
+                     label, {"Charm": 1/phys_Ds, "Bottom": 1/phys_MBs})
+
+
+def inv_mD_corrected(ed, options):
+    mdata = ed.D_mass()
+    m1 = ed.heavy_m1
+    m2 = ed.heavy_m2
+    print m1, m2
+    exit(-1)
+    data = 1.0/(mdata +(m2 - m1))
+    label = "$1/(m_{hl} + m_2 - m_1)$"
+    if options.scale:
+        label += " [1/MeV]"
+    return (data.mean(), data.std(), label, {"Charm": 1.0/phys_D, "Bottom": 1.0/phys_MB})
+
+def inv_mhl_corrected(ed, options):
+    mdata = ed.get_mass("heavy-ud")
+    data = {}
+    for m, m12s in ed.ep.m12s.iteritems():
+        mkey = [k for k in mdata if m in k][0]
+        m1,m2 = m12s
+        print m1,m2
+        data[m] = (mdata[mkey] + (m2 - m1)*ed.scale )
+    label = "$1/(m_{hl} + m_2 - m_1)$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+        phys = {"Charm": phys_D, "Bottom": phys_MB}
+    return {m.split("_")[-1]: ((1/d).mean(), (1/d).std(), label, phys ) for m,d in data.iteritems()}
+
+
+def inv_mDs_corrected(ed, options):
+    mdata = ed.Ds_mass()
+    m1 = ed.heavy_m1
+    m2 = ed.heavy_m2
+    data = 1.0/(mdata +(m2 - m1))
+    label = "$1/(m_{hs} + m_2 - m_1)$"
+    if options.scale:
+        label += " [1/MeV]"
+    return (data.mean(), data.std(), label, {"Charm": 1.0/phys_Ds, "Bottom": 1.0/phys_MBs})
+
+def inv_mhs_corrected(ed, options):
+    mdata = ed.get_mass("heavy-s")
+    data = {}
+    for m, m12s in ed.ep.m12s.iteritems():
+        mkey = [k for k in mdata if m in k][0]
+        m1,m2 = m12s
+        print m1,m2
+        data[m] = (mdata[mkey] + (m2 - m1)*ed.scale )
+    label = "$1/(m_{hs} + m_2 - m_1)$"
+    if ed.scale != 1.0:
+        label += " [1/MeV]"
+        phys = {"Charm": phys_Ds, "Bottom": phys_MBs}
+    return {m.split("_")[-1]: ((1/d).mean(), (1/d).std(), label, phys ) for m,d in data.iteritems()}
