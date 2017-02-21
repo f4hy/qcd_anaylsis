3b684035992a6a30056f3d638336c1dd59edd5a4
Brendan Fahy <bmfahy@gmail.com>
2017-02-21 17:15:41 +0900

    added nlo to x fpi mpi fits

--- global_model2_0/pion_chiral_model.py
+++ global_model2_0/pion_chiral_model.py
@@ -64,6 +64,104 @@ class fpi_x_NLO(Model):
         sqr_diff2 = (self.bstrapdata("fpi") - F)**2
         return np.sum(sqr_diff2 / self.var2)
 
+class fpi_mpi_x_NLO(Model):
+
+    def __init__(self, ensemble_datas, options):
+
+        Model.__init__(self, ensemble_datas, options)
+        self.data["fpi"] = self.make_array("fpi")
+        self.data["mpi"] = self.make_array("pion_mass")
+        self.data["mK"] = self.make_array("kaon_mass")
+
+        try:
+            self.var1 = (((self.data["mpi"]**2).std(1)/self.consts["renorm_qmass"])**2
+                         + (self.consts["residual_error"]*self.data["mpi"].mean(1) / self.consts["qmass"])**2)
+            self.var2 = (self.data["fpi"]).var(1)
+
+        except IndexError:
+            self.var1 = np.NaN
+            self.var2 = np.NaN
+
+        try:
+            if "fpi" in options.ydata:
+                self.plotmode = "fpi"
+            else:
+                self.plotmode = "mpisqr"
+        except AttributeError as e:
+            self.plotmode = ""
+
+        self.label = "Linear continuum fit"
+
+        self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
+        self.update_paramdict("B", 2736.37, 1.0, limits=(2500, None))
+
+        self.update_paramdict("Lambda3", 579.59, 50.0)
+        self.update_paramdict("Lambda4",  967.35, 140.55, limits=(0, None))
+        self.update_paramdict("gamma_1", -0.1, 0.1)
+        self.update_paramdict("gamma_2", -0.1, 0.1)
+        self.update_paramdict("gamma_s1", 0.0, 0.0001)
+        self.update_paramdict("gamma_s2", 0.0, 0.0001)
+
+        self.contlim_args = ["F_0", "B", "Lambda3", "Lambda4" ]
+        self.finbeta_args = ["F_0", "B", "Lambda3", "Lambda4", "gamma_1", "gamma_2"]
+
+    def degrees_of_freedom(self):
+        """
+        Return the degrees of freedom of the model. Overload this method if different if different
+        """
+        logging.info("data")
+        datapoints = [d.shape[0] for d in self.data.values()]
+        ndata = datapoints[0] * 2 # Twice the dof because combined fit to two data sets
+
+        fixed_parms = [p for p in self.params if "fix" in p and self.params[p]]
+        Nparams = inspect.getargspec(self.m).args[1:]
+        Nfree_params = len(Nparams) - len(fixed_parms)
+        dof = float(ndata - Nfree_params)
+        logging.info("DOF {}, data {}, free {}".format(dof, ndata, Nfree_params))
+        return dof
+
+    def m(self, F_0, B, Lambda3, Lambda4, gamma_1, gamma_2, gamma_s1, gamma_s2):
+
+        qm = self.consts["renorm_qmass"]
+        Msqr = B*(qm+qm)
+        x = Msqr/(8*(np.pi**2)*(F_0**2))
+        arg3 = (Lambda3**2)/Msqr
+        arg4 = (Lambda4**2)/Msqr
+
+        Mss = (2.0 * self.bstrapdata("mK")**2 - self.bstrapdata("mpi")**2)
+        phys_Mss = (2.0 * (pv.phys_kaon**2)) - (pv.phys_pion**2)
+        delta_Mss = Mss - phys_Mss
+
+        asqr = (self.consts["lat"]**2)
+
+        delta_M = (1+gamma_1*asqr+gamma_s1*delta_Mss)
+        delta_F = (1+gamma_2*asqr+gamma_s2*delta_Mss)
+
+        Mpisqr = delta_M * 2*B*(1.0-0.5*x*np.log(arg3))
+        Fpi = delta_F * F_0 * (1.0 + x*np.log(arg4))
+
+        return Mpisqr, Fpi
+
+    def plot_fit(self, x, F_0, B, Lambda3, Lambda4):
+        """ Override the normal plotter, this needs to not call m in this case """
+        Msqr = x * (8 * (np.pi**2) * (F_0**2))
+        arg3 = (Lambda3**2)/Msqr
+        arg4 = (Lambda4**2)/Msqr
+
+        if self.plotmode == "fpi":
+            return F_0 * (1.0 + x*np.log(arg4))
+        else:
+            return 2 * B * (1.0-0.5*x*np.log(arg3))
+
+    def sqr_diff(self, F_0, B, Lambda3, Lambda4, gamma_1, gamma_2, gamma_s1, gamma_s2):
+
+        M, F = self.m(F_0, B, Lambda3, Lambda4, gamma_1, gamma_2, gamma_s1, gamma_s2)
+        mdata = self.bstrapdata("mpi")**2 / self.consts["renorm_qmass"]
+        sqr_diff1 = (mdata - M)**2
+        sqr_diff2 = (self.bstrapdata("fpi") - F)**2
+        return np.sum(sqr_diff1 / self.var1) + np.sum(sqr_diff2 / self.var2)
+
+
 
 class fpi_mpi_x_NNLO(Model):
 
