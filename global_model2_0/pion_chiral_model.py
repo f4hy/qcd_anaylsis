6c6c853b19a2fd20f7c60372329b4526cea28845
Brendan Fahy <bmfahy@gmail.com>
2017-03-02 17:00:27 +0900

    fix up chiral models

--- global_model2_0/pion_chiral_model.py
+++ global_model2_0/pion_chiral_model.py
@@ -23,7 +23,7 @@ class fpi_x_NLO(Model):
             self.var1 = np.NaN
             self.var2 = np.NaN
 
-        self.label = "Linear continuum fit"
+        self.label = "NLO"
 
         self.update_paramdict("F_0", 110.0, 1.0, limits=(0, None))
         self.update_paramdict("B", 3000.0, 3.0, limits=(2000, None))
@@ -90,7 +90,7 @@ class fpi_mpi_x_NLO(Model):
         except AttributeError as e:
             self.plotmode = ""
 
-        self.label = "Linear continuum fit"
+        self.label = "NLO"
 
         self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
         self.update_paramdict("B", 2736.37, 1.0, limits=(2500, None))
@@ -109,16 +109,7 @@ class fpi_mpi_x_NLO(Model):
         """
         Return the degrees of freedom of the model. Overload this method if different if different
         """
-        logging.info("data")
-        datapoints = [d.shape[0] for d in self.data.values()]
-        ndata = datapoints[0] * 2 # Twice the dof because combined fit to two data sets
-
-        fixed_parms = [p for p in self.params if "fix" in p and self.params[p]]
-        Nparams = inspect.getargspec(self.m).args[1:]
-        Nfree_params = len(Nparams) - len(fixed_parms)
-        dof = float(ndata - Nfree_params)
-        logging.info("DOF {}, data {}, free {}".format(dof, ndata, Nfree_params))
-        return dof
+        return Model.degrees_of_freedom(self,data_multiply=2.0)
 
     def m(self, F_0, B, Lambda3, Lambda4, gamma_1, gamma_2, gamma_s1, gamma_s2):
 
@@ -189,7 +180,7 @@ class fpi_mpi_x_NNLO(Model):
         except AttributeError as e:
             self.plotmode = ""
 
-        self.label = "Linear continuum fit"
+        self.label = "NNLO"
 
         self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
         self.update_paramdict("B", 2736.37, 1.0, limits=(2500, None))
@@ -211,16 +202,7 @@ class fpi_mpi_x_NNLO(Model):
         """
         Return the degrees of freedom of the model. Overload this method if different if different
         """
-        logging.info("data")
-        datapoints = [d.shape[0] for d in self.data.values()]
-        ndata = datapoints[0] * 2 # Twice the dof because combined fit to two data sets
-
-        fixed_parms = [p for p in self.params if "fix" in p and self.params[p]]
-        Nparams = inspect.getargspec(self.m).args[1:]
-        Nfree_params = len(Nparams) - len(fixed_parms)
-        dof = float(ndata - Nfree_params)
-        logging.info("DOF {}, data {}, free {}".format(dof, ndata, Nfree_params))
-        return dof
+        return Model.degrees_of_freedom(self,data_multiply=2.0)
 
     def m(self, F_0, B, Lambda3, Lambda4, Lambda12, km, kf, gamma_1, gamma_2, gamma_s1, gamma_s2):
 
@@ -326,20 +308,20 @@ class fpi_mpi_xi_inverse_NNLO(Model):
         except AttributeError as e:
             self.plotmode = ""
 
-        self.label = "Linear continuum fit"
+        self.label = "NNLO"
 
-        self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
-        self.update_paramdict("B", 2736.37, 1.0, limits=(0, None))
+        self.update_paramdict("F_0", 121.8, 5.1, limits=(0, None))
+        self.update_paramdict("B", 2850.37, 20.0, limits=(0, None))
 
-        self.update_paramdict("Lambda3", 579.59, 50.0, limits=(0, None))
-        self.update_paramdict("Lambda4",  967.35, 140.55, limits=(0, None))
+        self.update_paramdict("Lambda3", 700.59, 50.0, limits=(0, None))
+        self.update_paramdict("Lambda4",  1000.35, 140.55, limits=(0, None))
         self.update_paramdict("Lambda12", 20.0, 0.1, limits=(0, None), fix=True)
-        self.update_paramdict("cm", 1.0, 0.1)
-        self.update_paramdict("cf", -12.0, 0.1)
-        self.update_paramdict("gamma_1", -0.1, 0.1)
-        self.update_paramdict("gamma_2", -0.1, 0.1)
-        self.update_paramdict("gamma_s1", 0.0, 0.0001)
-        self.update_paramdict("gamma_s2", 0.0, 0.0001)
+        self.update_paramdict("cm", 2.0, 1.1)
+        self.update_paramdict("cf", -12.0, 1.1)
+        self.update_paramdict("gamma_1", 3.0, 1.0)
+        self.update_paramdict("gamma_2", -3.0, 1.0)
+        self.update_paramdict("gamma_s1", 0.0, 0.000001)
+        self.update_paramdict("gamma_s2", 0.0, 0.000001)
 
         self.contlim_args = ["F_0", "B", "Lambda3", "Lambda4", "Lambda12", "cm", "cf", ]
         self.finbeta_args = ["F_0", "B", "Lambda3", "Lambda4", "Lambda12", "cm", "cf", "gamma_1", "gamma_2"]
@@ -348,21 +330,11 @@ class fpi_mpi_xi_inverse_NNLO(Model):
         """
         Return the degrees of freedom of the model. Overload this method if different if different
         """
-        logging.info("data")
-        datapoints = [d.shape[0] for d in self.data.values()]
-        ndata = datapoints[0] * 2 # Twice the dof because combined fit to two data sets
-
-        fixed_parms = [p for p in self.params if "fix" in p and self.params[p]]
-        Nparams = inspect.getargspec(self.m).args[1:]
-        Nfree_params = len(Nparams) - len(fixed_parms)
-        dof = float(ndata - Nfree_params)
-        logging.info("DOF {}, data {}, free {}".format(dof, ndata, Nfree_params))
-        return dof
+        return Model.degrees_of_freedom(self, data_multiply=2.0)
 
     def m(self, xi, F_0, B, Lambda3, Lambda4, Lambda12, cm, cf, gamma_1, gamma_2, gamma_s1, gamma_s2):
 
         mpisqr = self.bstrapdata("mpi")**2
-        print mpisqr
         arg3 = (Lambda3**2)/mpisqr
         arg4 = (Lambda4**2)/mpisqr
 
@@ -396,20 +368,17 @@ class fpi_mpi_xi_inverse_NNLO(Model):
 
         delta_M = (1+gamma_1*asqr+gamma_s1*delta_Mss)
         delta_F = (1+gamma_2*asqr+gamma_s2*delta_Mss)
+        Mpisqr = delta_M * 2*B / (1.0 + 0.5*xi*np.log(arg3) -5.0/8.0*(xi*lnOmegaM)**2 + cm*(xi**2))
 
-        Mpisqr = delta_M * 2*B / (1.0 - 0.5*xi*np.log(arg3) -5.0/8.0*(xi*lnOmegaM)**2 + cm*(xi**2))
-
-        Fpi = delta_F * F_0 / (1.0 + xi*np.log(arg4) - 1.0/4.0*(xi*lnOmegaF)**2 + cf*(xi**2))
+        Fpi = delta_F * F_0 / (1.0 - xi*np.log(arg4) - 1.0/4.0*(xi*lnOmegaF)**2 + cf*(xi**2))
 
         return Mpisqr, Fpi
 
     def plot_fit(self, xi, F_0, B, Lambda3, Lambda4, Lambda12, cm, cf):
         """ Override the normal plotter, this needs to not call m in this case """
+        self.data["mpi"] = np.array([[pv.phys_pion]])
+        self.data["mK"] = np.array([[pv.phys_kaon]])
         Mpisqr, Fpi = self.m(xi, F_0, B, Lambda3, Lambda4, Lambda12, cm, cf, 0, 0 ,0 ,0)
-        print Fpi
-        print Mpisqr
-        exit(-1)
-        return Fpi
         if self.plotmode == "fpi":
             return Fpi
         else:
@@ -452,12 +421,12 @@ class fpi_mpi_xi_NNLO(Model):
         except AttributeError as e:
             self.plotmode = ""
 
-        self.label = "Linear continuum fit"
+        self.label = "NNLO"
 
-        self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
+        self.update_paramdict("F_0", 130.8, 0.1, limits=(0, None))
         self.update_paramdict("B", 2736.37, 1.0, limits=(2500, None))
 
-        self.update_paramdict("c3", 3000.0, 1.0, limits=(-1000, 4000))
+        self.update_paramdict("c3", 1453.0, 1.0, limits=(-1000, 4000))
         self.update_paramdict("c4", -1000.0, 1.0)
         self.update_paramdict("alpha", -4000.0, 1.0)
         self.update_paramdict("beta", 2000.0, 1.0)
@@ -475,16 +444,7 @@ class fpi_mpi_xi_NNLO(Model):
         """
         Return the degrees of freedom of the model. Overload this method if different if different
         """
-        logging.info("data")
-        datapoints = [d.shape[0] for d in self.data.values()]
-        ndata = datapoints[0] * 2  # Twice the dof because combined fit to two data sets
-
-        fixed_parms = [p for p in self.params if "fix" in p and self.params[p]]
-        Nparams = inspect.getargspec(self.m).args[1:]
-        Nfree_params = len(Nparams) - len(fixed_parms)
-        dof = float(ndata - Nfree_params)
-        logging.info("DOF {}, data {}, free {}".format(dof, ndata, Nfree_params))
-        return dof
+        return Model.degrees_of_freedom(self, data_multiply=2.0)
 
     def m(self, xi, F_0, B, c3, c4, alpha, beta, ellphys, gamma_1, gamma_2, gamma_s1, gamma_s2):
 
@@ -542,9 +502,9 @@ class fpi_xi_NLO(Model):
             self.var2 = np.NaN
 
 
-        self.label = "Linear continuum fit"
+        self.label = "NLO"
 
-        self.update_paramdict("F_0", 117.8, 0.1, limits=(0, None))
+        self.update_paramdict("F_0", 130.8, 0.1, limits=(0, None))
         self.update_paramdict("c4", 0.0, 1.0)
 
         self.update_paramdict("gamma_2", -0.1, 0.1)
@@ -576,6 +536,68 @@ class fpi_xi_NLO(Model):
         sqr_diff2 = (self.bstrapdata("fpi") - F)**2
         return np.sum(sqr_diff2 / self.var2)
 
+class fpi_xi_inverse_NLO(Model):
+
+    def __init__(self, ensemble_datas, options):
+
+        Model.__init__(self, ensemble_datas, options)
+        self.data["fpi"] = self.make_array("fpi")
+        self.data["mpi"] = self.make_array("pion_mass")
+        self.data["mK"] = self.make_array("kaon_mass")
+        self.data["xi"] = self.make_array("xi")
+
+        try:
+            self.var1 = (((self.data["mpi"]**2).std(1)/self.consts["renorm_qmass"])**2
+                         + (self.consts["residual_error"]*self.data["mpi"].mean(1) / self.consts["qmass"])**2)
+            self.var2 = (self.data["fpi"]).var(1)
+
+        except IndexError:
+            self.var1 = np.NaN
+            self.var2 = np.NaN
+
+        self.label = "NLO"
+
+        self.update_paramdict("F_0", 130.8, 0.1, limits=(0, None))
+        self.update_paramdict("Lambda4", 1100.0, 1.0)
+
+        self.update_paramdict("gamma_2", -0.1, 0.1)
+        self.update_paramdict("gamma_s2", 0.0, 0.0001)
+
+        self.contlim_args = ["F_0", "Lambda4"]
+        self.finbeta_args = ["F_0", "Lambda4", "gamma_2"]
+
+    def plot_fit(self, xi, F_0, Lambda4):
+        """ Override the normal plotter, this needs to not call m in this case """
+        self.data["mpi"] = np.array([[pv.phys_pion]])
+        self.data["mK"] = np.array([[pv.phys_kaon]])
+        return self.m( xi, F_0, Lambda4)
+
+    def m(self, xi, F_0, Lambda4, gamma_2=0, gamma_s2=0):
+
+        mpisqr = self.bstrapdata("mpi")**2
+        arg4 = (Lambda4**2)/mpisqr
+
+        Mss = (2.0 * self.bstrapdata("mK")**2 - self.bstrapdata("mpi")**2)
+        phys_Mss = (2.0 * (pv.phys_kaon**2)) - (pv.phys_pion**2)
+        delta_Mss = Mss - phys_Mss
+
+        asqr = (self.consts["lat"]**2)
+
+        delta_F = (1+gamma_2*asqr+gamma_s2*delta_Mss)
+
+        Fpi = delta_F * (F_0 / (1 - xi*np.log(arg4)))
+
+        return Fpi
+
+
+    def sqr_diff(self, F_0, Lambda4, gamma_2, gamma_s2):
+
+        xi = self.bstrapdata("xi")
+        F = self.m(xi, F_0, Lambda4, gamma_2, gamma_s2)
+        sqr_diff2 = (self.bstrapdata("fpi") - F)**2
+        return np.sum(sqr_diff2 / self.var2)
+
+
 class mpi_xi_NLO(Model):
 
     def __init__(self, ensemble_datas, options):
@@ -595,10 +617,10 @@ class mpi_xi_NLO(Model):
             self.var1 = np.NaN
             self.var2 = np.NaN
 
-        self.label = "Linear continuum fit"
+        self.label = "NLO"
 
-        self.update_paramdict("B", 117.8, 0.1, limits=(0, None))
-        self.update_paramdict("c3", 0.0, 1.0)
+        self.update_paramdict("B", 2835.0, 0.1, limits=(0, None))
+        self.update_paramdict("c3", 1453.0, 1.0)
 
         self.update_paramdict("gamma_1", -0.1, 0.1)
         self.update_paramdict("gamma_s1", 0.0, 0.0001)
@@ -624,6 +646,68 @@ class mpi_xi_NLO(Model):
 
         xi = self.bstrapdata("xi")
         M = self.m(xi, B, c3, gamma_1, gamma_s1)
+        mdata = self.bstrapdata("mpi")**2 / self.consts["renorm_qmass"]
+        sqr_diff1 = (mdata - M)**2
+        return np.sum(sqr_diff1 / self.var1)
+
+class mpi_xi_inverse_NLO(Model):
+
+    def __init__(self, ensemble_datas, options):
+
+        Model.__init__(self, ensemble_datas, options)
+        self.data["fpi"] = self.make_array("fpi")
+        self.data["mpi"] = self.make_array("pion_mass")
+        self.data["mK"] = self.make_array("kaon_mass")
+        self.data["xi"] = self.make_array("xi")
+
+        try:
+            self.var1 = (((self.data["mpi"]**2).std(1)/self.consts["renorm_qmass"])**2
+                         + (self.consts["residual_error"]*self.data["mpi"].mean(1) / self.consts["qmass"])**2)
+            self.var2 = (self.data["fpi"]).var(1)
+
+        except IndexError:
+            self.var1 = np.NaN
+            self.var2 = np.NaN
+
+        self.label = "NLO"
+
+        self.update_paramdict("B", 2835.0, 0.1, limits=(0, None))
+        self.update_paramdict("Lambda3", 1453.0, 1.0)
+
+        self.update_paramdict("gamma_1", -0.1, 0.1)
+        self.update_paramdict("gamma_s1", 0.0, 0.0001)
+
+        self.contlim_args = ["B", "Lambda3"]
+        self.finbeta_args = ["B", "Lambda3", "gamma_1"]
+
+
+    def m(self, xi, B, Lambda3, gamma_1=0, gamma_s1=0):
+
+        mpisqr = self.bstrapdata("mpi")**2
+        arg3 = (Lambda3**2)/mpisqr
+
+        Mss = (2.0 * self.bstrapdata("mK")**2 - self.bstrapdata("mpi")**2)
+        phys_Mss = (2.0 * (pv.phys_kaon**2)) - (pv.phys_pion**2)
+        delta_Mss = Mss - phys_Mss
+
+        asqr = (self.consts["lat"]**2)
+        delta_M = (1+gamma_1*asqr+gamma_s1*delta_Mss)
+
+        Mpisqr = delta_M * (2*B/(1.0+0.5*xi*np.log(arg3)))
+
+        return Mpisqr
+
+    def plot_fit(self, xi, B, Lambda3):
+        """ Override the normal plotter, this needs to not call m in this case """
+        self.data["mpi"] = np.array([[pv.phys_pion]])
+        self.data["mK"] = np.array([[pv.phys_kaon]])
+        return self.m( xi, B, Lambda3)
+
+
+    def sqr_diff(self, B, Lambda3, gamma_1, gamma_s1):
+
+        xi = self.bstrapdata("xi")
+        M = self.m(xi, B, Lambda3, gamma_1, gamma_s1)
         mdata = self.bstrapdata("mpi")**2 / self.consts["renorm_qmass"]
         sqr_diff1 = (mdata - M)**2
         return np.sum(sqr_diff1 / self.var1)
